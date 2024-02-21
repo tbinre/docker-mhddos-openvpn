eb3dee95c43e7122ecf18b205d317051a3bc1274
Trashbin <tbinre@protonmail.com>
2024-02-21 16:38:47 +0200

    Make session time configurable

--- restart.py
+++ restart.py
@@ -3,7 +3,10 @@ import signal
 import random
 import time
 
-restart_countdown = random.randint(600, 3600)
+SESSION_TIME_MIN = os.getenv('SESSION_TIME_MIN', 2700)
+SESSION_TIME_MAX = os.getenv('SESSION_TIME_MAX', 10800)
+
+restart_countdown = random.randint(SESSION_TIME_MIN, SESSION_TIME_MAX)
 
 # Sleep for randomly picked seconds
 time.sleep(restart_countdown)
